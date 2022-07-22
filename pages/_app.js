c6a7e7a332ae9dabb7f1da6d820468b2eb2a369c
Lennard Dorst <79471472+0w9@users.noreply.github.com>
2022-07-22 21:50:19 +0200

    Fixing GTag Tracking Id.

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,14 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return 
       <>
-        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-0SQ7C0EDQQ`} />
 
         <Script strategy="lazyOnload">
             {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                gtag('config', G-0SQ7C0EDQQ', {
                 page_path: window.location.pathname,
                 });
             `}
